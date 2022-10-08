11bd80203ebcd9c595c942d5f7b48c121a27b46c
Zhari7 <meneseszharick29@gmail.com>
2022-10-08 18:37:09 -0500

    MAP

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 arr.forEach(function callback(currentValue, index, array) {
     // tu iterador
 }[ thisArg]);
+
+// Declaración de un Map (un HashMap) con clave "Integer" y Valor "String". Las claves pueden ser de cualquier tipo de objetos, aunque los más utilizados como clave son los objetos predefinidos de Java como String, Integer, Double ... !!!!CUIDADO los Map no permiten datos atómicos
+Map<Integer, String> nombreMap = new HashMap<Integer, String>();
+nombreMap.size(); // Devuelve el numero de elementos del Map
+nombreMap.isEmpty(); // Devuelve true si no hay elementos en el Map y false si si los hay
+nombreMap.put(K clave, V valor); // Añade un elemento al Map
+nombreMap.get(K clave); // Devuelve el valor de la clave que se le pasa como parámetro o 'null' si la clave no existe
+nombreMap.clear(); // Borra todos los componentes del Map
+nombreMap.remove(K clave); // Borra el par clave/valor de la clave que se le pasa como parámetro
+nombreMap.containsKey(K clave); // Devuelve true si en el map hay una clave que coincide con K
+nombreMap.containsValue(V valor); // Devuelve true si en el map hay un Valor que coincide con V
+nombreMap.values(); // Devuelve una "Collection" con los valores del Map
